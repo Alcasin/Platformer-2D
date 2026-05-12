83665aa295858670b83b240051a97859824c871f
Alper Kırca <alpr.1496.6941@gmail.com>
2026-05-12 20:08:33 +0300

    feat: add pause menu and sound manager system

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,14 +3,31 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Game Over")]
     [SerializeField] private GameObject gameOverScreen;
     [SerializeField] private AudioClip gameOverSound;
+
+    [Header("Pause")]
+    [SerializeField] private GameObject pauseScreen;
 
     private void Awake()
     {
         gameOverScreen.SetActive(false);
+        pauseScreen.SetActive(false);
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseScreen.activeInHierarchy)
+                PauseGame(false);
+            else
+                PauseGame(true);
+        }
+    }
+
+    #region Game Over
     public void GameOver()
     {
         gameOverScreen.SetActive(true);
@@ -35,4 +52,26 @@ public class UIManager : MonoBehaviour
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
     }
+    #endregion
+
+    #region Pause
+    public void PauseGame(bool status)
+    {
+        pauseScreen.SetActive(status);
+
+        if (status)
+            Time.timeScale = 0;
+        else
+            Time.timeScale = 1;
+    }
+    public void SoundVolume()
+    {
+        SoundManager.instance.ChangeSoundVolume(0.2f);
+    }
+
+    public void MusicVolume()
+    {
+        SoundManager.instance.ChangeMusicVolume(0.2f);
+    }
+    #endregion
 }
